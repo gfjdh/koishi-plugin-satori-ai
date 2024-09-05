867c62210e3bf8f8ea30934f501f1b564cf19872
gfjdh1 <2556123498@qq.com>
2024-09-05 17:52:40 +0800

    bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -120,7 +120,7 @@ class SAt extends Sat {
     {
       notExists = await isTargetIdExists(this.ctx, session.userId); // 该群中的该用户是否签到过
       if (notExists) return session.text('commands.sat.messages.notExists')
-      if (!notExists) user = await this.ctx.database.get('p_system', { userid: session.userId });
+      user = await this.ctx.database.get('p_system', { userid: session.userId });
       if (user[0].favorability < -50 && user[0].favorability > -900) return session.text('commands.sat.messages.block1')
       const englishLettersCount = (prompt.match(/[a-zA-Z]/g) || []).length;
       if (user[0].favorability < 50 && englishLettersCount > 8) return session.text('commands.sat.messages.tooManyEnglishLetters')// 如果 prompt 中有超过八个英文字母则拦截
